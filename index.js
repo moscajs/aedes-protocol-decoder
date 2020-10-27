18f921227a0ab4d8277937cf57dafdb8fa70ffb0
getlarge <ed@getlarge.eu>
2020-10-27 09:03:59 +0100

    replace spread operator

--- index.js
+++ index.js
@@ -43,86 +43,84 @@ function getProtoIpFamily (ipFamily) {
   return 0
 }
 
-function extractHttpDetails (req, socket) {
-  const details = {}
+function extractHttpDetails (req, socket, proto = {}) {
   const headers = req && req.headers ? req.headers : null
   if (headers) {
     if (headers['x-forwarded-for']) {
       const addresses = forwarded(req)
-      details.ipAddress = headers['x-real-ip'] ? headers['x-real-ip'] : addresses[addresses.length - 1]
-      details.serverIpAddress = addresses[0]
+      proto.ipAddress = headers['x-real-ip'] ? headers['x-real-ip'] : addresses[addresses.length - 1]
+      proto.serverIpAddress = addresses[0]
     }
     if (headers['x-real-ip']) {
-      details.ipAddress = headers['x-real-ip']
+      proto.ipAddress = headers['x-real-ip']
     }
-    details.port = socket._socket.remotePort
-    details.ipFamily = getProtoIpFamily(socket._socket.remoteFamily)
-    details.isWebsocket = true
+    proto.port = socket._socket.remotePort
+    proto.ipFamily = getProtoIpFamily(socket._socket.remoteFamily)
+    proto.isWebsocket = true
   }
-  return details
+  return proto
 }
 
-function extractProxyDetails (buffer) {
-  const details = {}
+function extractProxyDetails (buffer, proto = {}) {
   let proxyProto
   if (isValidV1ProxyProtocol(buffer)) {
     proxyProto = proxyProtocol.V1BinaryProxyProtocol.parse(buffer)
     if (proxyProto && proxyProto.source && proxyProto.data) {
-      details.ipFamily = getProtoIpFamily(proxyProto.inetProtocol)
-      details.ipAddress = proxyProto.source.ipAddress
-      details.port = proxyProto.source.port
-      details.serverIpAddress = proxyProto.destination.ipAddress
-      details.data = proxyProto.data
-      details.isProxy = 1
+      proto.ipFamily = getProtoIpFamily(proxyProto.inetProtocol)
+      proto.ipAddress = proxyProto.source.ipAddress
+      proto.port = proxyProto.source.port
+      proto.serverIpAddress = proxyProto.destination.ipAddress
+      proto.data = proxyProto.data
+      proto.isProxy = 1
     }
   } else if (isValidV2ProxyProtocol(buffer)) {
     proxyProto = proxyProtocol.V2ProxyProtocol.parse(buffer)
     if (proxyProto && proxyProto.proxyAddress && proxyProto.data) {
       if (proxyProto.proxyAddress instanceof proxyProtocol.IPv4ProxyAddress) {
-        details.ipAddress = proxyProto.proxyAddress.sourceAddress.address.join('.')
-        details.port = proxyProto.proxyAddress.sourcePort
-        details.serverIpAddress = proxyProto.proxyAddress.destinationAddress.address.join('.')
-        details.ipFamily = 4
+        proto.ipAddress = proxyProto.proxyAddress.sourceAddress.address.join('.')
+        proto.port = proxyProto.proxyAddress.sourcePort
+        proto.serverIpAddress = proxyProto.proxyAddress.destinationAddress.address.join('.')
+        proto.ipFamily = 4
       } else if (proxyProto.proxyAddress instanceof proxyProtocol.IPv6ProxyAddress) {
-        details.ipAddress = parseIpV6Array(proxyProto.proxyAddress.sourceAddress.address)
-        details.port = proxyProto.proxyAddress.sourcePort
-        details.serverIpAddress = parseIpV6Array(proxyProto.proxyAddress.destinationAddress.address)
-        details.ipFamily = 6
+        proto.ipAddress = parseIpV6Array(proxyProto.proxyAddress.sourceAddress.address)
+        proto.port = proxyProto.proxyAddress.sourcePort
+        proto.serverIpAddress = parseIpV6Array(proxyProto.proxyAddress.destinationAddress.address)
+        proto.ipFamily = 6
       }
-      details.isProxy = 2
-      details.data = Buffer.isBuffer(proxyProto.data) ? proxyProto.data : Buffer.from(proxyProto.data)
+      proto.isProxy = 2
+      proto.data = Buffer.isBuffer(proxyProto.data) ? proxyProto.data : Buffer.from(proxyProto.data)
     }
   }
-  return details
+  return proto
 }
 
-function extractSocketDetails (socket) {
-  const details = {}
+function extractSocketDetails (socket, proto = {}) {
   if (socket._socket && socket._socket.address) {
-    details.isWebsocket = true
-    details.ipAddress = socket._socket.remoteAddress
-    details.port = socket._socket.remotePort
-    details.serverIpAddress = socket._socket.address().address
-    details.ipFamily = getProtoIpFamily(socket._socket.remoteFamily)
+    proto.isWebsocket = true
+    proto.ipAddress = socket._socket.remoteAddress
+    proto.port = socket._socket.remotePort
+    proto.serverIpAddress = socket._socket.address().address
+    proto.ipFamily = getProtoIpFamily(socket._socket.remoteFamily)
   } else if (socket.address) {
-    details.ipAddress = socket.remoteAddress
-    details.port = socket.remotePort
-    details.serverIpAddress = socket.address().address
-    details.ipFamily = getProtoIpFamily(socket.remoteFamily)
+    proto.ipAddress = socket.remoteAddress
+    proto.port = socket.remotePort
+    proto.serverIpAddress = socket.address().address
+    proto.ipFamily = getProtoIpFamily(socket.remoteFamily)
   }
-  return details
+  return proto
 }
 
 function protocolDecoder (conn, buffer, req) {
-  let proto = {}
+  const proto = {}
   if (!buffer) return proto
   const socket = conn.socket || conn
   proto.isProxy = 0
   proto.isWebsocket = false
-  proto = { ...proto, ...extractHttpDetails(req, socket), ...extractProxyDetails(buffer) }
+  extractHttpDetails(req, socket, proto)
+  extractProxyDetails(buffer, proto)
 
   if (!proto.ipAddress) {
-    proto = { ...proto, ...extractSocketDetails(socket) }
+    extractSocketDetails(socket, proto)
   }
 
   return proto
